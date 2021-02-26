6e3c5a5742e3f77d7c066d9eeb3700ddd625bb76
Yaroslav Gavrilenko <y.gavrilenko@luckywarepro.com>
2021-02-26 11:10:33 +0200

    Add whole interval case

--- app/src/androidTest/java/MusIntervalRecognizerTest.java
+++ app/src/androidTest/java/MusIntervalRecognizerTest.java
@@ -1,6 +1,7 @@
 
 import android.util.Log;
 
+import com.ichi2.apisample.MusInterval;
 import com.ichi2.apisample.MusIntervalRecognizer;
 
 import org.junit.Test;
@@ -61,6 +62,52 @@ public class MusIntervalRecognizerTest {
             } catch (Exception e) {
                 failed.add(new String[]{"crash (1)", sampleName});
             }
+        }
+        Log.i("Passed", passed.size() + "\n" + String.join("\n", passed));
+        String failMsg = failed.size() + "\n";
+        for (String[] s : failed) {
+            failMsg += String.join(" - ", s) + "\n";
+        }
+        Log.i("Failed", failMsg);
+        double successRate = (double) passed.size() / (double) sampleNames.length;
+        assertEquals(successRate, 1.0, 0);
+    }
+
+    @Test
+    public void testMusInterval() {
+        File samplesDir = new File(samplesDirPath);
+        String[] sampleNames = samplesDir.list();
+        ArrayList<String> passed = new ArrayList<>();
+        ArrayList<String[]> failed = new ArrayList<>();
+        for (String sampleName : sampleNames) {
+            try {
+                String[] notes = new MusIntervalRecognizer(samplesDirPath + sampleName).getNotes();
+                if (notes.length == 2) {
+                    String note1 = notes[0];
+                    String note2 = notes[1];
+                    String sampleData[] = sampleName.split("_");
+                    String sampleStartNote = sampleData[0];
+                    String sampleDirection = sampleData[1];
+                    String recognizedDirection = MusIntervalRecognizer.getDirection(note1, note2);
+                    String sampleInterval = sampleData[3];
+                    int recognizedDistance = MusIntervalRecognizer.getDistance(note1, note2);
+                    String recognizedInterval = recognizedDistance < MusInterval.Fields.Interval.VALUES.length ?
+                            MusInterval.Fields.Interval.VALUES[recognizedDistance] :
+                            String.valueOf(recognizedDistance);
+                    if (sampleStartNote.equals(note1) &&
+                            sampleDirection.equals(recognizedDirection) &&
+                            sampleInterval.equals(recognizedInterval)) {
+                        passed.add(sampleName);
+                    } else {
+                        failed.add(new String[]{note1, recognizedDirection, recognizedInterval, sampleName});
+                    }
+                } else {
+                    failed.add(new String[]{String.valueOf(notes.length), sampleName});
+                }
+            } catch (Exception e) {
+                failed.add(new String[]{"crash", sampleName});
+            }
+
         }
         Log.i("Passed", passed.size() + "\n" + String.join("\n", passed));
         String failMsg = failed.size() + "\n";
