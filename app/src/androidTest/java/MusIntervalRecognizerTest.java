ebce85fe7a07e88eadcbe57ea1a012be1d17cf37
Yaroslav Gavrilenko <y.gavrilenko@luckywarepro.com>
2021-02-26 11:00:37 +0200

    Add additional logs to instrumental tests

--- app/src/androidTest/java/MusIntervalRecognizerTest.java
+++ app/src/androidTest/java/MusIntervalRecognizerTest.java
@@ -13,61 +13,62 @@ import java.util.ArrayList;
 
 public class MusIntervalRecognizerTest {
 
+    final static String samplesDirPath = "/storage/emulated/0/MusicIntervals2Anki/";
+
     @Test
     public void testStartNote() {
-        final String samplesDirPath = "/storage/emulated/0/MusicIntervals2Anki/";
         File samplesDir = new File(samplesDirPath);
         String[] sampleNames = samplesDir.list();
         ArrayList<String> passed = new ArrayList<>();
-        ArrayList<String> failed = new ArrayList<>();
+        ArrayList<String[]> failed = new ArrayList<>();
         for (String sampleName : sampleNames) {
             try {
                 String[] notes = new MusIntervalRecognizer(samplesDirPath + sampleName).getNotes();
-
                 if (notes[0].equals(sampleName.split("_")[0])) {
                     passed.add(sampleName);
                 } else {
-                    failed.add(sampleName);
+                    failed.add(new String[]{notes[0], sampleName});
                 }
             } catch (Exception e) {
-                failed.add(sampleName);
+                failed.add(new String[]{"crash", sampleName});
             }
 
         }
         Log.i("Passed", passed.size() + "\n" + String.join("\n", passed));
-        Log.i("Failed", failed.size() + "\n" + String.join("\n", failed));
+        String failMsg = failed.size() + "\n";
+        for (String[] s : failed) {
+            failMsg += String.join(" - ", s) + "\n";
+        }
+        Log.i("Failed", failMsg);
         double successRate = (double) passed.size() / (double) sampleNames.length;
-        Log.i("Success rate", String.format("%.2f", successRate));
         assertEquals(successRate, 1.0, 0);
     }
 
     @Test
     public void testNotesLength() {
-        final String samplesDirPath = "/storage/emulated/0/MusicIntervals2Anki/";
         File samplesDir = new File(samplesDirPath);
         String[] sampleNames = samplesDir.list();
         ArrayList<String> passed = new ArrayList<>();
-        ArrayList<String> failed = new ArrayList<>();
-        ArrayList<String> crashed = new ArrayList<>();
+        ArrayList<String[]> failed = new ArrayList<>();
         for (String sampleName : sampleNames) {
             try {
                 String[] notes = new MusIntervalRecognizer(samplesDirPath + sampleName).getNotes();
-
                 if (notes.length == 2) {
                     passed.add(sampleName);
                 } else {
-                    failed.add(sampleName);
+                    failed.add(new String[]{String.valueOf(notes.length), sampleName});
                 }
             } catch (Exception e) {
-                crashed.add(sampleName);
+                failed.add(new String[]{"crash (1)", sampleName});
             }
-
         }
         Log.i("Passed", passed.size() + "\n" + String.join("\n", passed));
-        Log.i("Failed", failed.size() + "\n" + String.join("\n", failed));
-        Log.i("Crashed", crashed.size() + "\n" + String.join("\n", crashed));
+        String failMsg = failed.size() + "\n";
+        for (String[] s : failed) {
+            failMsg += String.join(" - ", s) + "\n";
+        }
+        Log.i("Failed", failMsg);
         double successRate = (double) passed.size() / (double) sampleNames.length;
-        Log.i("Success rate", String.format("%.2f", successRate));
         assertEquals(successRate, 1.0, 0);
     }
 }
