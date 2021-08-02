1ab9192a0dbd7269f56d262f38a58f34ec3ad2a7
Yaroslav Gavrilenko <y.gavrilenko@luckywarepro.com>
2021-08-02 17:27:29 +0300

    Remove redundant suppression

--- app/src/main/java/com/luckywarepro/musicintervals2anki/ui/MainActivity.java
+++ app/src/main/java/com/luckywarepro/musicintervals2anki/ui/MainActivity.java
@@ -203,7 +203,6 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
 
     private final static Map<Integer, String> CHECK_INTERVAL_ID_VALUES = new HashMap<>();
     static {
-        //noinspection ConstantConditions
         if (CHECK_INTERVAL_IDS.length != MusInterval.Fields.Interval.VALUES.length) {
             throw new AssertionError();
         }
