6d55da62430f4ca0eb38b56ab09c7a61c4aeaba3
Yaroslav Gavrilenko <y.gavrilenko@luckywarepro.com>
2021-08-04 13:32:11 +0300

    Add about activity

---
+++ app/src/main/java/com/ichi2/apisample/ui/AboutActivity.java
@@ -0,0 +1,16 @@
+package com.ichi2.apisample.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ichi2.apisample.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
