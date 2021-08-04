d6ce4180a067204828b3aa658b2ec49c4fcc0677
Yaroslav Gavrilenko <y.gavrilenko@luckywarepro.com>
2021-08-04 13:48:28 +0300

    Add toolbar

--- app/src/main/java/com/ichi2/apisample/ui/AboutActivity.java
+++ app/src/main/java/com/ichi2/apisample/ui/AboutActivity.java
@@ -1,10 +1,13 @@
 package com.ichi2.apisample.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 import com.ichi2.apisample.R;
+
+import java.util.Objects;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -12,5 +15,9 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        Toolbar toolbar = findViewById(R.id.toolbarAbout);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
     }
 }
