1824701a30bf2d80b55e30d273a467b707a3d7b2
Yaroslav Gavrilenko <y.gavrilenko@luckywarepro.com>
2021-08-09 13:11:18 +0300

    Update toolbar

--- app/src/main/java/com/luckywarepro/musicintervals2anki/ui/MainActivity2.java
+++ app/src/main/java/com/luckywarepro/musicintervals2anki/ui/MainActivity2.java
@@ -1,11 +1,18 @@
 package com.luckywarepro.musicintervals2anki.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.luckywarepro.musicintervals2anki.R;
+import com.luckywarepro.musicintervals2anki.ui.settings.SettingsActivity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -17,6 +24,10 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Toolbar toolbarMain = findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbarMain);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -28,4 +39,29 @@ public class MainActivity2 extends AppCompatActivity {
         NavigationUI.setupWithNavController(navView, navController);
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.actionSettings) {
+            startActivity(new Intent(MainActivity2.this, SettingsActivity.class));
+            return true;
+        } else if (itemId == R.id.actionHelp) {
+            Uri uri = Uri.parse(getString(R.string.uri_readme));
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+            return true;
+        } else if (itemId == R.id.actionAbout) {
+            startActivity(new Intent(MainActivity2.this, AboutActivity.class));
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
 }
